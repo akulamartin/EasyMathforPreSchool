4a0b08865907ddb6dbff96227145288f4bead3fa
akulamartin <martin.akula@gmail.com>
2020-10-15 21:12:07 +0300

    chore : added a resources.

--- app/src/main/java/com/example/easymathforpreschool/Score.java
+++ app/src/main/java/com/example/easymathforpreschool/Score.java
@@ -1,6 +1,14 @@
 package com.example.easymathforpreschool;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Score extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
 }
--- app/src/main/java/com/example/easymathforpreschool/operations/Addition.java
+++ app/src/main/java/com/example/easymathforpreschool/operations/Addition.java
@@ -1,6 +1,16 @@
 package com.example.easymathforpreschool.operations;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.easymathforpreschool.R;
+
 public class Addition extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
 }
