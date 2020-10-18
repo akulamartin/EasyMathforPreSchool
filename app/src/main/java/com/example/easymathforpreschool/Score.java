74afed18fb0f9a7e4ecc40e2e2e2fcf9ecd97d73
akulamartin <martin.akula@gmail.com>
2020-10-18 17:58:05 +0300

    chore : renamed files.

--- app/src/main/java/com/example/easymathforpreschool/Score.java
+++ app/src/main/java/com/example/easymathforpreschool/Score.java
@@ -9,6 +9,6 @@ public class Score extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_score);
+        setContentView(R.layout.activity_my_score);
     }
 }
