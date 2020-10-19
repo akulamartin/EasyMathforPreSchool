359d60022539ee84e1c26d42ad4b63552f10c812
akulamartin <martin.akula@gmail.com>
2020-10-19 19:05:45 +0300

    chore : adding methods.

--- app/src/main/java/com/example/easymathforpreschool/MainActivity.java
+++ app/src/main/java/com/example/easymathforpreschool/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.easymathforpreschool;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonAbout = findViewById(R.id.button_about);
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent aboutIntent = new Intent(getApplicationContext(), AboutApp.class);
+                startActivity(aboutIntent);
+            }
+        });
     }
 }
